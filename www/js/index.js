85625a8edabaf286e25d2577c3c8aac071f5537a
THEODORA H. CHENG <tcheng@us.ibm.com>
2016-02-25 00:26:47 -0600

    draft sample

--- www/js/index.js
+++ www/js/index.js
@@ -1,19 +1,3 @@
-/**
-* Copyright 2016 IBM Corp.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-    
 var Messages = {
     // Add here your messages for the default language.
     // Generate a similar file with a language suffix containing the translated messages.
@@ -27,101 +11,117 @@ var wlInitOptions = {
 
 // Called automatically after MFP framework initialization by WL.Client.init(wlInitOptions).
 function wlCommonInit(){
-	// Common initialization code goes here
+    //MFP APIs should only be called within wlCommonInit() or after it has been called, to ensure that the APIs have loaded properly
+}
 
-    app.receivedEvent('deviceready');
+document.addEventListener('deviceready', function() {
     MFPPush.initialize(function(successResponse) {
             alert("Successfully intialized");
             MFPPush.registerNotificationsCallback(notificationReceived);
     }, function(failureResponse) {
             alert("Failed to initialize");
-    });		
+    });
+}, false);
 
+function isPushSupported() {
+    MFPPush.isPushSupported(function(successResponse) {
+        alert("Push Supported: " + successResponse);
+    }, function(failureResponse) {
+        alert("Failed to get push support status");
+    });
+}
 
-    function isPushSupported() {
-		MFPPush.isPushSupported(function(successResponse) {
-                alert("Push Supported: " + successResponse);
-        },function(failureResponse) {
-                alert("Failed to get push support status");
-        });
+var notificationReceived = function(message) {
+    alert(JSON.stringify(message));
+};
 
-        var notificationReceived = function(message) {
-            alert(JSON.stringify(message));
-        };
-        
-    function registerDevice() {
+function registerDevice() {
     // Optional parameter, but must follow this format
-        var settings = {
-            ios: {
-                alert: true,
-                badge: true,
-                sound: true
-            }
-        };
-
-        MFPPush.registerDevice(settings,function(successResponse) {
-                alert("Successfully registered");  
-                enableButtons();
-            },function(failureResponse) {
-                alert("Failed to register");
-            });
-
+    var settings = {
+    ios: {
+        alert: true,
+        badge: true,
+        sound: true
     }
+    };
 
-    function getTags() {
-        MFPPush.getTags(function(tags) {
-            alert(JSON.stringify(tags));
-        }, function(){
-            alert("Failed to get tags");
-        });
-    }
+    MFPPush.registerDevice(settings, null,
+                           function(successResponse) {
+                           alert("Successfully registered");
+                           enableButtons();
+                           },
+                           function(failureResponse) {
+                           alert("Failed to register");
+                           }
+                           );
 
-    function getSubscriptions() {
-        MFPPush.getSubscriptions(function(subscriptions) {
-            alert(JSON.stringify(subscriptions));
-        }, function(){
-            alert("Failed to get subscriptions");
-        });
-    }
+}
 
-    function subscribe() {
-        var tags = ['sample-tag1','sample-tag2']
-        MFPPush.subscribe(tags, function(tags) {
-            alert("Subscribed successfully");
-        },function(){
-            alert("Failed to subscribe");
-        });
-    }
+function getTags() {
+    MFPPush.getTags(function(tags) {
+                    alert(JSON.stringify(tags));
+                    },
+                    function(){
+                    alert("Failed to get tags");
+                    }
+                    );
+}
 
-    function unsubscribe() {
-        var tags = ['sample-tag1','sample-tag2']
-        MFPPush.unsubscribe(tags, function(tags) {
-        alert("Unsubscribed successfully");
-        }, function(){
-            alert("Failed to unsubscribe");
-        });
-    }
+function getSubscriptions() {
+    MFPPush.getSubscriptions(function(subscriptions) {
+                             alert(JSON.stringify(subscriptions));
+                             },
+                             function(){
+                             alert("Failed to get subscriptions");
+                             }
+                             );
+}
 
-    function unregisterDevice() {
-        MFPPush.unregisterDevice(function(successResponse) {
-            alert("Unregistered successfully");
-            disableButtons();
-        }, function(){
-            alert("Failed to unregister");
-        });
-    }
+function subscribe() {
+    var tags = ['sample-tag1','sample-tag2']
+    MFPPush.subscribe(tags,
+                      function(tags) {
+                      alert("Subscribed successfully");
+                      },
+                      function(){
+                      alert("Failed to subscribe");
+                      }
+                      );
+}
 
-    function enableButtons() {
-        document.getElementById("subscribe").disabled = false;
-        document.getElementById("getsubscriptions").disabled = false;
-        document.getElementById("unsubscribe").disabled = false;
-        document.getElementById("unregister").disabled = false;
-    }
-  
-    function disableButtons(){
-        document.getElementById("subscribe").disabled = true;
-        document.getElementById("getsubscriptions").disabled = true;
-        document.getElementById("unsubscribe").disabled = true;
-        document.getElementById("unregister").disabled = true; 
-    }
+function unsubscribe() {
+    var tags = ['sample-tag1','sample-tag2']
+    MFPPush.unsubscribe(tags,
+                        function(tags) {
+                        alert("Unsubscribed successfully");
+                        },
+                        function(){
+                        alert("Failed to unsubscribe");
+                        }
+                        );
+}
+
+function unregisterDevice() {
+    MFPPush.unregisterDevice(function(successResponse) {
+                             alert("Unregistered successfully");
+                             disableButtons();
+                             },
+                             function(){
+                             alert("Failed to unregister");
+                             }
+                             );
+}
+
+function enableButtons() {
+    document.getElementById("subscribe").disabled = false;
+    document.getElementById("getsubscriptions").disabled = false;
+    document.getElementById("unsubscribe").disabled = false;
+    document.getElementById("unregister").disabled = false;
+}
+
+function disableButtons(){
+    document.getElementById("subscribe").disabled = true;
+    document.getElementById("getsubscriptions").disabled = true;
+    document.getElementById("unsubscribe").disabled = true;
+    document.getElementById("unregister").disabled = true;
 }
