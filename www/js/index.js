e3a78267a06b5c29aef62dff388b5f0f1f7663a3
THEODORA H. CHENG <tcheng@us.ibm.com>
2016-02-25 01:07:16 -0600

    reformatted as well as updated registserdevice api not to have "options" and "null" in parameters

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,19 @@
+/**
+* Copyright 2016 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 var Messages = {
     // Add here your messages for the default language.
     // Generate a similar file with a language suffix containing the translated messages.
@@ -16,10 +32,10 @@ function wlCommonInit(){
 
 document.addEventListener('deviceready', function() {
     MFPPush.initialize(function(successResponse) {
-            alert("Successfully intialized");
-            MFPPush.registerNotificationsCallback(notificationReceived);
+         alert("Successfully intialized");
+         MFPPush.registerNotificationsCallback(notificationReceived);
     }, function(failureResponse) {
-            alert("Failed to initialize");
+         alert("Failed to initialize");
     });
 }, false);
 
@@ -36,25 +52,13 @@ var notificationReceived = function(message) {
 };
 
 function registerDevice() {
-    // Optional parameter, but must follow this format
-    var settings = {
-    ios: {
-        alert: true,
-        badge: true,
-        sound: true
-    }
-    };
 
-    MFPPush.registerDevice(settings, null,
-                           function(successResponse) {
-                           alert("Successfully registered");
-                           enableButtons();
-                           },
-                           function(failureResponse) {
-                           alert("Failed to register");
-                           }
-                           );
-
+    MFPPush.registerDevice(function(successResponse) {
+        alert("Successfully registered");
+        enableButtons();
+    }, function(failureResponse) {
+        alert("Failed to register");
+    });
 }
 
 function getTags() {
